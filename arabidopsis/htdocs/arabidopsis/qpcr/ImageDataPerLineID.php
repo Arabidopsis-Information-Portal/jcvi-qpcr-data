4e706fe8fbcd17451e5069954d6c939e35a469b7
eriksf <erik@jcvi.org>
2015-08-13 17:46:49 -0400

    change input param to line_id now returns URL to image as well

--- arabidopsis/htdocs/arabidopsis/qpcr/ImageDataPerLineID.php
+++ arabidopsis/htdocs/arabidopsis/qpcr/ImageDataPerLineID.php
@@ -10,7 +10,19 @@
         return False;
     }
 
-    $search_line_id = filter_input(INPUT_GET, 'line', FILTER_SANITIZE_STRING);
+    function selfURLBase() {
+        $s = empty($_SERVER["HTTPS"]) ? "" : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+        $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s;
+        $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":" .$_SERVER["SERVER_PORT"]);
+        $app_base = dirname($_SERVER["PHP_SELF"]);
+        return $protocol."://".$_SERVER["SERVER_NAME"].$port.$app_base;
+    }
+
+    function strleft($s1, $s2) {
+        return substr($s1, 0, strpos($s1, $s2));
+    }
+
+    $search_line_id = filter_input(INPUT_GET, 'line_id', FILTER_SANITIZE_STRING);
 
     if ( count(preg_split("/-/", $search_line_id)) === 4 ) {
         list($plate, $well, $dip_num, $plant_num) = preg_split("/-/", $search_line_id);
@@ -46,7 +58,8 @@
          $po_name = $row["name"];
 
          if (!search_images($images, $image_id)) {
-             $images[] = array('image_id' => $image_id);
+             $images[] = array('image_id' => $image_id,
+                               'image_url' => selfURLBase()."/get_image.php?image_id=".$image_id);
          }
          $po_codes[$image_id][] = array('po_code' => $po_code, 'po_name' => $po_name, 'expression' => $expression);
     }
